8c7af75a8da969a3fbcdfdfbdcb42c5f0d55bc14
dharanisrirk-tech <dharanisrirk@gmail.com>
2026-02-11 22:24:37 +0530

    Add files via upload

---
+++ stremailusername.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include<string.h>
+int main() {
+    char email[100];
+    int i = 0;
+    scanf("%s", email);
+    while (email[i] != '\0' && email[i] != '@') {
+        printf("%c", email[i]);
+        i++;
+    }
+    return 0;
+}
