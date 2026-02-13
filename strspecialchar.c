d61e478b235dc97cbd65f92ab8f7db7a7fa24f99
dharanisrirk-tech <dharanisrirk@gmail.com>
2026-02-13 22:04:46 +0530

    Add files via upload

---
+++ strspecialchar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() {
+    char str[100];
+    int count = 0, i = 0;
+    scanf("%s", str);
+    while (str[i] != '\0') {
+        if (str[i] == '@' || str[i]=='#' ||str[i]=='$' || str[i]=='%' )
+            count++;
+        i++;
+    }
+    printf("%d", count);
+    return 0;
+}
